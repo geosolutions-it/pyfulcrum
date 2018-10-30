30ee9f8e39c1acef6e7eaf2dca3cd0db64a624d6
Cezary Statkiewicz <cezio@thelirium.net>
2018-10-30 19:54:50 +0100

    travis build update

--- scripts/travis/travis-build.bash
+++ scripts/travis/travis-build.bash
@@ -24,6 +24,7 @@ pip install pygdal==2.2.2.3 # $(gdal-config --version)
 pip install -r requirements.txt
 pip install -e .
 
+aptitude search postgresql
 sudo service postgresql start
 
 sudo -u postgres psql -p 5433 -c "create role pyfulcrum superuser login password 'pyfulcrum';"
