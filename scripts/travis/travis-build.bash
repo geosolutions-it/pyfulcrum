3049639720f5a7a5e9c90080bd5aa1b61544719d
Cezary Statkiewicz <cezio@thelirium.net>
2018-10-31 00:17:52 +0100

    travis build update

--- scripts/travis/travis-build.bash
+++ scripts/travis/travis-build.bash
@@ -14,13 +14,12 @@ sudo add-apt-repository 'http://archive.ubuntu.com/ubuntu/'
 sudo add-apt-repository 'http://archive.ubuntu.com/ubuntu/ universe'
 sudo add-apt-repository 'http://archive.ubuntu.com/ubuntu/ multiverse'
 sudo apt-get -qq --fix-missing update
-# sudo aptitude remove -y postgresql-9.5-postgis-2.4
-# sudo aptitude install -y libgdal20 python3-dev python3-pip python3-wheel
-# sudo apt-get install libgdal-dev
+sudo aptitude remove -y postgresql-9.5-postgis-2.3
+sudo aptitude install -y libgdal20 python3-dev python3-pip python3-wheel
+sudo apt-get install libgdal-dev
 
-sudo dpkg -l postgresql
 
-sudo aptitude install -t ubuntugis-testing postgresql-9.5-postgis-2.4
+sudo aptitude install postgresql-9.5-postgis-2.3
 
 
 # PostGIS 2.1 already installed on Travis
