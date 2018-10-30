703a9c183c4eea839696ddbf25562408a8959f04
Cezary Statkiewicz <cezio@thelirium.net>
2018-10-30 22:18:56 +0100

    travis build update

--- scripts/travis/travis-build.bash
+++ scripts/travis/travis-build.bash
@@ -25,6 +25,7 @@ pip install pygdal==2.2.2.3 # $(gdal-config --version)
 pip install -r requirements.txt
 pip install -e .
 
+aptitude search gdal
 # sudo aptitude install postgis
 # postgresql-9.6-postgis-2.3
 sudo service postgresql start
