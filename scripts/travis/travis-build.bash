a0b7f3d1d816adbdd608cca3ed6356066ad94b72
Cezary Statkiewicz <cezio@thelirium.net>
2018-11-13 18:24:19 +0100

    travis: remove pg repo, use ubuntugis

--- scripts/travis/travis-build.bash
+++ scripts/travis/travis-build.bash
@@ -15,8 +15,8 @@ sudo add-apt-repository 'http://archive.ubuntu.com/ubuntu/'
 sudo add-apt-repository 'http://archive.ubuntu.com/ubuntu/ universe'
 sudo add-apt-repository 'http://archive.ubuntu.com/ubuntu/ multiverse'
 sudo apt-get -qq --fix-missing update
-sudo aptitude search postgis
-sudo aptitude install -y libgdal20 python3-dev python3-pip python3-wheel postgresql-9.6-postgis-2.4
+sudo aptitude remove postgresql postgis
+sudo aptitude install -y libgdal20 python3-dev python3-pip python3-wheel postgresql-9.6-postgis-2.3-scripts
 sudo apt-get install libgdal-dev
 
 #sudo apt-cache madison postgresql-9.5-postgis-2.3
