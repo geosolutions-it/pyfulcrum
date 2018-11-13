c3b2e21cd568592cd2d12197988477dbd0fbe8fa
Cezary Statkiewicz <cezio@thelirium.net>
2018-11-13 21:34:31 +0100

    travis: lib+web

--- scripts/travis/travis-run.sh
+++ scripts/travis/travis-run.sh
@@ -3,4 +3,4 @@
 # echo "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
 # sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
 # sudo service jetty restart
-TEST_DATABASE_URI=postgresql://pyfulcrum:pyfulcrum@localhost/pyfulcrum_test pytest  --cov=pyfulcrum.lib --cov=pyfulcrum.web $@
+TEST_DB_URI=postgresql://pyfulcrum:pyfulcrum@localhost/pyfulcrum_test pytest  --cov=pyfulcrum.lib --cov=pyfulcrum.web $@
