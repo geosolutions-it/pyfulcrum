55f2e1dec7a60bd0a5a3225bf5d2fecaf0a7245a
Cezary Statkiewicz <cezio@thelirium.net>
2018-11-13 21:28:41 +0100

    travis: lib+web

--- scripts/travis/travis-build.bash
+++ scripts/travis/travis-build.bash
@@ -40,12 +40,19 @@ cd lib
 pip install --upgrade pip
 pip install 'pygdal<2.2.3' # $(gdal-config --version)
 pip install -r requirements.txt
+pip install -r test-requirements.txt
 pip install -e .
+
+cd ../web
+pip install -r requirements.txt
+pip install -r test-requirements.txt
+pip install -e .
+
 # sudo pg_ctlcluster 9.2 main stop
 # sudo pg_ctlcluster 9.3 main start
 
-sudo -u postgres psql -c "create role pyfulcrum superuser login password 'pyfulcrum';"
-sudo -u postgres psql -c "create database pyfulcrum_test owner pyfulcrum;"
-sudo -u postgres psql -d pyfulcrum_test -c 'create extension postgis;'
+#sudo -u postgres psql -c "create role pyfulcrum superuser login password 'pyfulcrum';"
+#sudo -u postgres psql -c "create database pyfulcrum_test owner pyfulcrum;"
+#sudo -u postgres psql -d pyfulcrum_test -c 'create extension postgis;'
 
 echo "travis-build.bash is done."f
